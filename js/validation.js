4a7d8e05e5eb0c5584a7e609f7268f6fcaf557ae
splantapot <joaovictor1007@outlook.com.br>
2024-12-03 10:29:22 -0300

    End pick image

--- js/validation.js
+++ js/validation.js
@@ -13,5 +13,5 @@ function validatePassword(event) {
 async function validateUserAdd(event) {
     event.preventDefault();
     getImageToForm();
-    // validatePassword(event);
+    validatePassword(event);
 }
